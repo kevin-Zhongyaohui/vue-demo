6e2332411122e3044bbb756597b10126f49e501f
kevzhong <shp2700@163.com>
2020-11-25 15:48:57 +0800

    2020-11-25 VUE 单页面适应屏幕

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <!--<img src="./assets/logo.png">-->
+    <router-view/>
+  </div>
+</template>
+
+
+<script>
+export default {
+
+  name: 'App',
+  data:{
+
+  },
+
+  mounted(){
+
+    kevin_resize({ size: [750, 1334], full: 1 })
+
+  },
+
+  methods:{
+
+
+
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,66 @@
+<template>
+
+  <div class="helloworld">
+
+  <div class="pic">
+
+    <img src="../assets/001.jpg" width="100%"/>
+
+    <div class="box"><img src="../assets/002.jpg" width="100%"/></div>
+
+  </div>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+  .pic{
+
+    position: absolute;
+    width: 1010px;
+    height: 1534px;
+    top: 0;
+    left: 50%;
+    margin-left: -505px;
+
+
+  }
+
+  .box{
+
+    position: absolute;
+    width: 633px;
+    height: 355px;
+    top: 844px;
+    left: 50%;
+    margin-left: -318px;
+
+  }
+</style>
